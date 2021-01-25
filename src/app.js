b430ed81c78dddb837a038cd404822fe1559d19f
phongho23 <ho.phongt@gmail.com>
2021-01-25 01:58:12 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const { v4: uuid } = require('uuid');
+const validateBearerToken = require('./validate-bearer-token')
+const errorHandler = require('./error-handler')
+const bookmarksRouter = require('./bookmarks/bookmarks-router')
+
+const app = express()
+
+app.use(morgan((NODE_ENV === 'production') 
+  ? 'tiny' 
+  : 'common', 
+  { skip: () => NODE_ENV === 'test' }
+  ))
+
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
+app.use(validateBearerToken)
+
+app.use(bookmarksRouter)
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+app.use(errorHandler)
+
+module.exports = app
